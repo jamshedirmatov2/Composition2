fdc6da7f46f254d813f883c283067d5b1105b7fc
jamshedirmatov2 <jamshedirmatov2@gmail.com>
2024-03-06 00:14:04 +0500

    Fragments basic

--- app/src/main/java/com/wigroup/composition2/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/wigroup/composition2/presentation/GameFinishedFragment.kt
@@ -1,18 +1,87 @@
 package com.wigroup.composition2.presentation
 
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
-import com.wigroup.composition2.R
+import androidx.fragment.app.FragmentManager
+import com.wigroup.composition2.databinding.FragmentGameFinishedBinding
+import com.wigroup.composition2.domain.entity.GameResult
 
 class GameFinishedFragment : Fragment() {
 
+    private lateinit var gameResult: GameResult
+
+    private var _binding: FragmentGameFinishedBinding? = null
+    private val binding: FragmentGameFinishedBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameFinishedBinding == null")
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        requireActivity().onBackPressedDispatcher
+            .addCallback(this, object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    retryGame()
+                }
+            })
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_game_finished, container, false)
+    ): View {
+        _binding = FragmentGameFinishedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonRetry.setOnClickListener { retryGame() }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun parseArgs() {
+        gameResult = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requireArguments().getSerializable(
+                KEY_GAME_RESULT,
+                GameResult::class.java
+            ) as GameResult
+        } else {
+            @Suppress("DEPRECATION")
+            requireArguments().getSerializable(KEY_GAME_RESULT) as GameResult
+        }
+    }
+
+    private fun retryGame() {
+        requireActivity().supportFragmentManager.popBackStack(
+            GameFragment.NAME,
+            FragmentManager.POP_BACK_STACK_INCLUSIVE
+        )
+    }
+
+    companion object {
+
+        private const val KEY_GAME_RESULT = "game_result"
+
+        fun newInstance(gameResult: GameResult): GameFinishedFragment =
+            GameFinishedFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(KEY_GAME_RESULT, gameResult)
+                }
+            }
     }
 }
